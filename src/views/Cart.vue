322583c7d2712821d426fd9f7cec4ad6ce7d6bd1
Alex Miller <amiller7@insight.com>
2020-01-16 13:01:46 -0500

    Cart view- Continue Shopping router link to home

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -18,10 +18,9 @@
 			<div class="row row_cart_buttons">
 				<div class="col">
 					<div class="cart_buttons d-flex flex-lg-row flex-column align-items-start justify-content-start">
-						<div class="button continue_shopping_button"><a href="#">Continue shopping</a></div>
+						<div class="button continue_shopping_button"><router-link :to="{ name: 'home' }">Continue shopping</router-link></div>
 						<div class="cart_buttons_right ml-lg-auto">
 							<div class="button clear_cart_button"><a href="#" v-on:click="this.clearCart">Clear cart</a></div>
-							<div class="button update_cart_button"><a href="#">Update cart</a></div>
 						</div>
 					</div>
 				</div>
